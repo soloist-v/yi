194998671ad305944350616e2c8a8addd5270c46
soloist <w1053904672@163.com>
2022-02-15 15:49:12 +0800

    update

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+import sys
+import shutil
+
+
+def get_install_requires():
+    install_requires = ["numpy"]
+    return install_requires
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        sys.argv.append("install")
+    setup(
+        name='yi',
+        version="1.0",
+        description='yi autograd',
+        long_description="工具接口",
+        keywords='yi',
+        packages=find_packages(),
+        package_data={'': ['*.*']},
+        python_requires=">=3.7",
+        install_requires=get_install_requires(),
+        zip_safe=False)
+    if sys.argv[1] != "bdist_wheel":
+        shutil.rmtree("dist", ignore_errors=True)
+    shutil.rmtree("build", ignore_errors=True)
+    shutil.rmtree("yi.egg-info", ignore_errors=True)
